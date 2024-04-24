mrbrdo/better_spree_localization

initial skeleton with rename script

Rakefile
@@ -1,0 +1,21 @@
+require 'bundler'
+Bundler::GemHelper.install_tasks
+
+require 'rspec/core/rake_task'
+require 'spree/testing_support/extension_rake'
+
+RSpec::Core::RakeTask.new
+
+task :default do
+  if Dir['spec/dummy'].empty?
+    Rake::Task[:test_app].invoke
+    Dir.chdir('../../')
+  end
+  Rake::Task[:spec].invoke
+end
+
+desc 'Generates a dummy app for testing'
+task :test_app do
+  ENV['LIB_NAME'] = 'gem_name'
+  Rake::Task['extension:test_app'].invoke
+end

lib/gem_name/engine.rb
@@ -1,0 +1,19 @@
+module GemName
+  class Engine < Rails::Engine
+    require 'spree/core'
+    isolate_namespace Spree
+    engine_name 'gem_name'
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    # use rspec for tests
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
+    def self.activate
+    end
+
+    config.to_prepare &method(:activate).to_proc
+  end
+end

lib/gem_name/railtie.rb
@@ -1,0 +1,11 @@
+module GemName
+  class Railtie < Rails::Railtie
+    initializer "gem_name.init" do |app|
+
+    end
+    
+    config.to_prepare do
+      
+    end
+  end
+end

lib/gem_name/version.rb
@@ -1,0 +1,3 @@
+module GemName
+  VERSION = '1.0.0'
+end

mrbrdo/spree_print_invoice_extras

Rakefile
@@ -1,0 +1,21 @@
+require 'bundler'
+Bundler::GemHelper.install_tasks
+
+require 'rspec/core/rake_task'
+require 'spree/testing_support/extension_rake'
+
+RSpec::Core::RakeTask.new
+
+task :default do
+  if Dir['spec/dummy'].empty?
+    Rake::Task[:test_app].invoke
+    Dir.chdir('../../')
+  end
+  Rake::Task[:spec].invoke
+end
+
+desc 'Generates a dummy app for testing'
+task :test_app do
+  ENV['LIB_NAME'] = 'spree_print_invoice_extras'
+  Rake::Task['extension:test_app'].invoke
+end

lib/spree_print_invoice_extras/engine.rb
@@ -1,0 +1,19 @@
+module SpreePrintInvoiceExtras
+  class Engine < Rails::Engine
+    require 'spree/core'
+    isolate_namespace Spree
+    engine_name 'spree_print_invoice_extras'
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    # use rspec for tests
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
+    def self.activate
+    end
+
+    config.to_prepare &method(:activate).to_proc
+  end
+end

lib/spree_print_invoice_extras/railtie.rb
@@ -1,0 +1,11 @@
+module SpreePrintInvoiceExtras
+  class Railtie < Rails::Railtie
+    initializer "spree_print_invoice_extras.init" do |app|
+
+    end
+    
+    config.to_prepare do
+      
+    end
+  end
+end

lib/spree_print_invoice_extras/version.rb
@@ -1,0 +1,3 @@
+module SpreePrintInvoiceExtras
+  VERSION = '1.0.0'
+end
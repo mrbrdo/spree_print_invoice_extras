3dd17f8a6857e16c196b55c909c6333b5120f832
mrbrdo <mrbrdo@gmail.com>
2022-04-20 01:38:24 +0200

    initial skeleton with rename script

---
+++ lib/spree_print_invoice_extras.rb
@@ -0,0 +1,5 @@
+require 'spree_core'
+require 'spree_extension'
+require 'spree_print_invoice_extras/version'
+# require 'spree_print_invoice_extras/engine'
+# require 'spree_print_invoice_extras/railtie' if defined?(Rails::Railtie)
---
+++ lib/spree_print_invoice_extras/railtie.rb
@@ -0,0 +1,11 @@
+module SpreePrintInvoiceExtras
+  class Railtie < Rails::Railtie
+    initializer "spree_print_invoice_extras.init" do |app|
+
+    end
+    
+    config.to_prepare do
+      
+    end
+  end
+end
